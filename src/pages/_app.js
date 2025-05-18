89cd3159dfef990fe7ab08b0f62d07d8b5ead2ac
H9660 <lohahussain0@gmail.com>
2025-05-18 12:28:22 +0530

    Minor changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 "use client";
-import { useRouter } from "next/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
@@ -11,14 +10,12 @@ import "../app/globals.css";
 import dotenv from "dotenv";
 dotenv.config("../.env.local");
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const noOtpPages = ["/register", "/login", "/otpverify"];
   return (
     <Provider store={store}>
       <ProvideChakra>
         <Navbar />
         <Component {...pageProps} />
-        {noOtpPages.includes(router.pathname) ? null : <Footer />}
+        <Footer />
       </ProvideChakra>
       <ToastContainer />
     </Provider>
